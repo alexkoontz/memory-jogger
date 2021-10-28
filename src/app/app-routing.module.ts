72ecc1ef4f4dae80d5edfa6d06d26ab2ade0b643
Alex <akoontz21@gmail.com>
2021-10-28 15:16:30 -0400

    Added Animate.css and user panel for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AddUserPanelComponent } from './components/add-user-panel/add-user-panel.component';
 import { LoginBoxComponent } from './components/login-box/login-box.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'login', component: LoginBoxComponent }
+  { path: 'login', component: LoginBoxComponent },
+  { path: 'user', component: UserComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AddUserPanelComponent } from './components/add-user-panel/add-user-pane
 import { DeleteUserPanelComponent } from './components/delete-user-panel/delete-user-panel.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { AdminComponent } from './components/admin/admin.component';
     AddUserPanelComponent,
     DeleteUserPanelComponent,
     HomeComponent,
-    AdminComponent
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
